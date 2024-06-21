ada2800415feee5fa66527e6ab6cea2528de47e0
klmadhu247 <klmadhu0709@gmail.com>
2024-06-21 19:39:38 +0530

    day18

---
+++ src/Flex.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import './flex.css'
+class Flex extends Component {
+    render()
+    {
+        return(
+            <div className='container-flex'>
+                <div>Hello</div>
+                <h1>world</h1>
+                <div>welcom</div>
+
+            </div>
+        )
+    }
+
+}
+export default Flex
